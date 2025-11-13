0b21f2fddd38e1dd348815cfe36ec0ef3e011550
gaurav0973 <codejourneypanda@gmail.com>
2025-11-13 21:29:38 +0530

    create favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 
+// favicon.ico
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
