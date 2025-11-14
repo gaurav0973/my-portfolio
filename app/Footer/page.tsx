66ceaba142032fcd57c283011e9462bccf062113
gaurav0973 <codejourneypanda@gmail.com>
2025-11-14 12:48:12 +0530

    add footer

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -1,10 +1,27 @@
+import Link from "next/link"
+
 function Footer() {
   return (
     <footer className="py-8 text-center text-slate-500 dark:text-gray-600 font-mono text-sm">
       <div className="flex justify-center items-center space-x-4">
-        <p>©2025 - gaurav maurya</p>
+        <div className="">
+          <div>
+          <p>©2025 - gaurav maurya</p>
+        </div>
+        <div>
+          <Link
+          href="https://github.com/gaurav0973/my-portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-slate-700 dark:hover:text-gray-400 transition"
+        >
+          Github
+        </Link>
+        </div>
+        </div>
       </div>
     </footer>
   )
 }
+
 export default Footer
